2499ee9392235fccf28c74fdfa1bd0700237e795
yang vieira <yangflix3@gmail.com>
2022-08-18 23:03:57 -0400

    :sparkles: class Appliance implemented

--- src/HouseholdAppliances/Appliance.cs
+++ src/HouseholdAppliances/Appliance.cs
@@ -1,5 +1,15 @@
 namespace HouseholdAppliances;
 
-public class Appliance
+public abstract class Appliance
 {
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public bool IsOn { get; set; }
+
+    public Appliance(string brand, string model, bool Ison)
+    {
+        Brand = brand;
+        Model = model;
+        IsOn = Ison;
+    }
 }
