407971f8564bdeacb04cf623252894952cad3013
yang vieira <yangflix3@gmail.com>
2022-08-19 20:43:31 -0400

    :zap: microwave

--- src/HouseholdAppliances/Microwave.cs
+++ src/HouseholdAppliances/Microwave.cs
@@ -1,9 +1,28 @@
+using static System.Console;
 namespace HouseholdAppliances;
 
-public class Microwave
+public class Microwave: Appliance, ICooker
 {
-    public Microwave(string alterarEsseNome, string alterarEsseNome2, int alterarEsseNome3, int alterarEsseNome4)
+
+   public int BoilingTime { get; set; }
+   public int MaximumTemperature { get; set; }
+    public Microwave(string brand, string model, int boilingTime, int maximumTemperature,  bool ison = false) : base(brand, model, ison)
     {
+       BoilingTime = boilingTime;
+       MaximumTemperature = maximumTemperature;
 
     }
+
+    public void Cook(string food)
+    {
+      if(!IsOn) throw new ArgumentException("the microwave is off");
+      WriteLine($"your {food} is in progress");
+    }
+
+    public void Heat(string food)
+    {
+      if(!IsOn) throw new ArgumentException("the microwave is off");
+      WriteLine($"your {food} is being heated");
+    }
+
 }
