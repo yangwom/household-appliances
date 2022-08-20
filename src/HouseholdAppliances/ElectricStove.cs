c8090b2ff4d301c478d860976740143509fe5b33
yang vieira <yangflix3@gmail.com>
2022-08-19 20:18:36 -0400

    :sparkles: electric stove

--- src/HouseholdAppliances/ElectricStove.cs
+++ src/HouseholdAppliances/ElectricStove.cs
@@ -1,9 +1,33 @@
+using static System.Console;
 namespace HouseholdAppliances;
 
-public class ElectricStove
+public class ElectricStove : Appliance, ICooker
 {
-    public ElectricStove(string alterarEsseNome, string alterarEsseNome2, int alterarEsseNome3, int alterarEsseNome4)
-    {
 
+
+    public int MaximumTemperature { get; set; }
+    public int BoilingTime { get; set; }
+
+
+    public ElectricStove(
+        string brand, string model, int maximumTemperature,
+        int boilingTime, bool Ison = false) : base(brand, model, Ison)
+    {
+        MaximumTemperature = maximumTemperature;
+        BoilingTime = boilingTime;
+    }
+
+
+    public void Cook(string food)
+    {
+        if (!IsOn) throw new Exception("o fogão não esta ligado");
+        WriteLine($"O {food} esta em andamento");
+    }
+
+
+    public void Bake(string food)
+    {
+        if (!IsOn) throw new Exception("o forno não esta ligado");
+        WriteLine($"o {food} esta assando");
     }
 }
